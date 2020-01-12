b66247de65c9000aaef1d2539349d36a191dd87f
Jamesblond007 <thefred9@gmail.com>
2020-01-11 22:03:59 -0700

    upstide

--- index.html/getdate.js
+++ index.html/getdate.js
@@ -34,4 +34,4 @@ let options = {
     };
     document.getElementById(
         "getdate2"
-    ).textContent = new Date().toLocaleDateString("en-US",options);
+    ).textContent = new Date().toLocaleDateString(options);
