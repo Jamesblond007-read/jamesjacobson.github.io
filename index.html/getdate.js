21c20bd16a9ec9a3890fa78f429e9030d6c0128e
Jamesblond007 <thefred9@gmail.com>
2020-01-18 13:49:13 -0700

    uu

--- index.html/getdate.js
+++ index.html/getdate.js
@@ -22,17 +22,30 @@ let months = [
     "December"
 ];
 
-try {
-    document.getElementById('currentYear').innerHTML = new Date().getFullYear();
-}
-catch (e) {
-    alert ["Your browser does not support this function"]
-}
-
-try {
-    document.getElementById('lastUpdated').innerHTML = new Date().toLocaleDateString("en-US") + " " + new Date().toLocaleTimeString("en-US");
-}
-catch (e) {
-    alert ["Your browser does not support this function"]
-}
-
+let d = new Date();
+let dayNames = daynames[d.getDate()];
+let monthsName = months[d.getMonth()];
+let year = d.getFullYear();
+let options = {
+        weekday: "short",
+        day: "numeric",
+        month: "short",
+        year: "numeric"
+    };
+    document.getElementById(
+        "getdate2"
+    ).textContent = new Date().toLocaleDateString(options);
+    try {
+        document.getElementById('currentYear').innerHTML = new Date().getFullYear();
+    }
+    catch (e) {
+        alert ["Your browser does not support this function"]
+    }
+    
+    try {
+        document.getElementById('lastUpdated').innerHTML = new Date().toLocaleDateString("en-US") + " " + new Date().toLocaleTimeString("en-US");
+    }
+    catch (e) {
+        alert ["Your browser does not support this function"]
+    }
+    
