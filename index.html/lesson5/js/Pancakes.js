2f823818cd3e041fb99fa4d0e04809aea8258bdf
Jamesblond007 <thefred9@gmail.com>
2020-02-08 10:43:48 -0700

    jj

--- index.html/lesson5/js/Pancakes.js
+++ index.html/lesson5/js/Pancakes.js
@@ -1,12 +1,4 @@
-var currentDate = new Date();
-var dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", 
-"Thursday", "Friday", "Saturday"];
-var day = new Date().getDay();
-document.getElementById("day1").innerText = dayNames[(day + 1) % 7];
-document.getElementById("day2").innerText = dayNames[(day + 2) % 7];
-document.getElementById("day3").innerText = dayNames[(day + 3) % 7];
-document.getElementById("day4").innerText = dayNames[(day + 4) % 7];
-document.getElementById("day5").innerText = dayNames[(day + 5) % 7];
-if (day != 5) {
-    document.getElementById("alert").style.display = "none";
+var Date = new Date();
+if (date.getday() == 5) {
+    document.getElementById("date").style.display = "none";
 }
--- index.html/lesson5/js/getdate.js
+++ index.html/lesson5/js/getdate.js
@@ -1,1 +1,6 @@
-document.write("Page was last modified on: " + document.lastModified);
+try {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentdate2').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
