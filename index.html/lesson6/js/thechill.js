2357ec816b37a6c7c0310eb0037119663b6990a7
Jamesblond007 <thefred9@gmail.com>
2020-02-15 12:25:17 -0700

    jj

--- index.html/lesson6/js/thechill.js
+++ index.html/lesson6/js/thechill.js
@@ -1,10 +1,9 @@
-const temp = document.getElementById('tem').textContent;
-const wind = document.getElementById('ws').textContent;
+const temp = parseFloat(document.getElementById('tem').textContent);
+const wind = parseFloat(document.getElementById('ws').textContent);
 
-var t = parseFloat(temp);
-var w = parseFloat(wind);
+
 if (t <= 50 && w >= 3){
-  const chilly = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+  const chilly = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16))).toFixed(2);
   document.getElementById('wic').textContent = chilly;
 }
 else{
