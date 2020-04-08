d4785b6b3e0691b7455371a2caee8e086a0b34c5
Jamesblond007 <thefred9@gmail.com>
2020-04-08 15:21:05 -0600

    jj

--- index.html/river_run/js/weatherJson.js
+++ index.html/river_run/js/weatherJson.js
@@ -7,20 +7,4 @@ fetch(apiURL)
 document.getElementById('currenttemp').textContent = Math.round(((jsObject.main.temp-273.15)*1.8)+32);
 document.getElementById('Temp').textContent = Math.round(((jsObject.main.temp_max-273.15)*1.8)+32);
 const temp = document.getElementById('currenttemp').textContent;
-const wind = document.getElementById('WindSpeed').textContent;
-const imagesrc = 'https://openweathermap.org/img/wn/' + jsObject.weather[0].icon + '@2x.png';
-document.getElementById('icon').setAttribute('src', imagesrc);
-
-var t = parseFloat(temp);
-var w = parseFloat(wind);
-
-if (t <= 50 && w >= 3){
-  const chilly = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16))).toFixed(2);
-  document.getElementById('WindChill').textContent = chilly;
-}
-else{
-    const chilly = ('N/A');
-    document.getElementById('WindChill').textContent = chilly;
-}
-
     });
